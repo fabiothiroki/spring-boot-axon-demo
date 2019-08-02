c28fbe16c640eb9b22b218c91f19d925ee191201
Fabio Hiroki <fabiothiroki@gmail.com>
2019-08-02 07:32:16 -0300

    rabbit config

---
+++ commandside/src/main/java/command/AmqpEventPublicationConfiguration.java
@@ -0,0 +1,36 @@
+package command;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmqpEventPublicationConfiguration {
+
+    @Value("${axon.amqp.exchange:command.events}")
+    String exchangeName;
+
+    @Bean
+    public Exchange exchange(){
+        return ExchangeBuilder.fanoutExchange(exchangeName).build();
+    }
+
+    @Bean
+    public Queue queue(){
+        return QueueBuilder.durable(exchangeName).build();
+    }
+
+    @Bean
+    public Binding binding(Queue queue, Exchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with("*").noargs();
+    }
+
+    @Autowired
+    public void configure(AmqpAdmin amqpAdmin, Exchange exchange, Queue queue, Binding binding){
+        amqpAdmin.declareExchange(exchange);
+        amqpAdmin.declareQueue(queue);
+        amqpAdmin.declareBinding(binding);
+    }
+}
