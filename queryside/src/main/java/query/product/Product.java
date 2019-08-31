a17d17fe88202b8659af388f4997da2c5fb53f32
Fabio Hiroki <fabiothiroki@gmail.com>
2019-08-31 08:52:48 -0300

    Create queryside files

---
+++ queryside/src/main/java/query/product/Product.java
@@ -0,0 +1,30 @@
+package query.product;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    public Product() {}
+
+    public Product(
+            final Long id,
+            final String name,
+            final int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    @Id
+    public Long id;
+
+    @Column
+    public String name;
+
+    @Column
+    public int quantity;
+
+}
---
+++ queryside/src/main/java/query/product/ProductController.java
@@ -0,0 +1,17 @@
+package query.product;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("Products!");
+    }
+
+}
